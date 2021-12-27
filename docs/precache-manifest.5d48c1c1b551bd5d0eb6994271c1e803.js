a9ab8eafd91b95f0e806bbe03ea482b9b719a456
Jhonnatan Suarez <jonathan.sm87@gmail.com>
2021-12-27 17:25:53 -0500

    docs agregado

---
+++ docs/precache-manifest.5d48c1c1b551bd5d0eb6994271c1e803.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eafed58826c53960a316d46e6e789ff3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da07fb7894a6b8a539c0",
+    "url": "/static/css/main.de641580.chunk.css"
+  },
+  {
+    "revision": "02217b6e85eb4875845c",
+    "url": "/static/js/2.4378ff7a.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/static/js/2.4378ff7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da07fb7894a6b8a539c0",
+    "url": "/static/js/main.6ed102d8.chunk.js"
+  },
+  {
+    "revision": "9d3929294d677b1b9ff6",
+    "url": "/static/js/runtime-main.aa9df6a3.js"
+  }
+]);
